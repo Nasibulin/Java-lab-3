9aa6039d6f2b3a8cc83d07feb12d22a995fd3058
Nasibulin <k.nasibulin@yandex.ru>
2018-10-17 14:55:32 +0300

    -

---
+++ src/test/java/DekanatDemoTest.java
@@ -0,0 +1,12 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: Nasibulin
+ * Date: 17.10.18
+ * Time: 15:44
+ * To change this template use File | Settings | File Templates.
+ */
+public class DekanatDemoTest {
+    public void testMain() throws Exception {
+
+    }
+}
